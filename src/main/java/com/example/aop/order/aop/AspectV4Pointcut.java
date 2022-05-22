53009791546a6a8058a904047b868118f3953254
최인석 <insuk@choeinseog-ui-MacBookPro.local>
2022-05-22 12:12:15 +0900

    first commit

---
+++ src/main/java/com/example/aop/order/aop/AspectV4Pointcut.java
@@ -0,0 +1,34 @@
+package com.example.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Slf4j
+@Aspect
+public class AspectV4Pointcut {
+
+    @Around("com.example.aop.order.aop.PointCuts.allOrder()")
+    public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("[log] {}", joinPoint.getSignature());
+        return joinPoint.proceed();
+    }
+
+    // com.example.aop.order 패키지와 그 하위 패키지이면서 클래스 이름 패턴이 *Service
+    @Around("com.example.aop.order.aop.PointCuts.orderAndService()")
+    public Object doTransaction(ProceedingJoinPoint joinPoint) throws Throwable {
+        try {
+            log.info("[트랜젝션 시작] {}", joinPoint.getSignature());
+            Object result = joinPoint.proceed();
+            log.info("[트랜젝션 커밋] {}", joinPoint.getSignature());
+            return result;
+        } catch (Exception e) {
+            log.info("[트랜젝션 롤백] {}", joinPoint.getSignature());
+            throw e;
+        } finally {
+            log.info("[리소스 릴리즈] {}", joinPoint.getSignature());
+        }
+    }
+}
