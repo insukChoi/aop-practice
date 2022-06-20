2b97ef433df0f0eeff5ecbf3c8771073e8b9288e
insuk <cis1725@gmail.com>
2022-06-20 23:16:49 +0900

    프록시 의존관계 주입과 타입캐스팅

---
+++ src/test/java/com/example/aop/proxyvs/ProxyCastingTest.java
@@ -0,0 +1,41 @@
+package com.example.aop.proxyvs;
+
+import com.example.aop.member.MemberService;
+import com.example.aop.member.MemberServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+
+@Slf4j
+public class ProxyCastingTest {
+
+    @Test
+    void jdkProxy() {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(false); // JDK 동적 프록시
+
+        // 프록시를 인터페이스로 캐스팅 성공
+        MemberService memberServiceProxy = (MemberService) proxyFactory.getProxy();
+
+        // JDK 동적 프록시를 구현 클래스로 캐스팅 시도 실패, ClassCastException 예외 발생
+        // -> JDK 동적 프록시는 인터페이스 기준으로 proxy를 만들기 때문에 구체클래스가 뭔지 모르기때문에 구체클래스에 캐스팅 불가
+        Assertions.assertThrows(ClassCastException.class, () -> {
+            MemberServiceImpl castingMemberService = (MemberServiceImpl) memberServiceProxy;
+        });
+    }
+
+    @Test
+    void cglibProxy() {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(true); // CGLIB 프록시
+
+        // 프록시를 인터페이스로 캐스팅 성공
+        MemberService memberServiceProxy = (MemberService) proxyFactory.getProxy();
+
+        // CGLIB 프록시를 구현 클래스로 캐스팅 시도 성공
+        MemberServiceImpl castingMemberService = (MemberServiceImpl) memberServiceProxy;
+    }
+}
---
+++ src/test/java/com/example/aop/proxyvs/code/ProxyDIAspectTest.java
@@ -0,0 +1,32 @@
+package com.example.aop.proxyvs.code;
+
+import com.example.aop.member.MemberService;
+import com.example.aop.member.MemberServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+//@SpringBootTest(properties = {"spring.aop.proxy-target-class=false"}) // JDK 동적 프록시
+@SpringBootTest(properties = {"spring.aop.proxy-target-class=true"}) // CGLIB 프록시 (default)
+@Import(ProxyDIAspect.class)
+class ProxyDIAspectTest {
+
+    @Autowired
+    private MemberService memberService;
+
+    @Autowired
+    private MemberServiceImpl memberServiceImpl;
+
+    @Test
+    void go() {
+        log.info("memberService class={}", memberService.getClass());
+        log.info("memberServiceImpl class={}", memberServiceImpl.getClass());
+        memberServiceImpl.hello("hello");
+    }
+
+}
